e3bf8137deae92691b54ab0d7ca6d3f66654d786
Amelia <me@amelia.fun>
2022-09-01 02:47:05 +0100

    discord v14 support.

--- bot/commands/control.js
+++ bot/commands/control.js
@@ -1,4 +1,4 @@
-const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+const { SelectMenuBuilder, SelectMenuOptionBuilder, ActionRowBuilder, ComponentType } = require('discord.js');
 
 module.exports = {
     name: 'control',
@@ -6,12 +6,18 @@ module.exports = {
     cooldown: 10,
 
     async execute(client, interaction) {
-        const menu = new MessageActionRow();
+        const menu = new ActionRowBuilder({
+            type: ComponentType.ActionRow
+        });
 
-        menu.addComponents(new MessageSelectMenu().setMinValues(1).setMaxValues(1).setPlaceholder('Nothing Selected').setCustomId('control'));
+        menu.addComponents([new SelectMenuBuilder().setCustomId('control').setMaxValues(1).setMinValues(1).setPlaceholder('Nothing Selected')]);
 
         for (let option of Object.keys(client.cfg.Options)) {
-            menu.components[0].options.push({ label: option, value: option, description: `${client.cfg.Options[option].Description}` });
+            menu.components[0].options.push(new SelectMenuOptionBuilder({
+                label: option,
+                value: option,
+                description: `${client.cfg.Options[option].Description}`
+            }));
         }
 
         await interaction.reply({ content: 'Choose an option from the menu below.', components: [menu], ephemeral: true });
